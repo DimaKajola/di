3fbc58ea021fb9859f13300ea1248eac71901c26
Dima Kajola <dima.kajola@gmail.com>
2020-11-22 14:25:48 +0200

    minor improvements

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="di-dimakajola",  # Replace with your own username
+    name="di",
     version="0.0.1",
     author="Dima Kajola",
     author_email="dima.kajola@gmail.com",
     description="A small package that resolves dependency injections via methods' annotations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/DimaKajola/di",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
